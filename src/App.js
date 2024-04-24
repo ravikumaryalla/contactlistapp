b92c5b8816adf990547b851a7cee21d15836606f
unknown <ravikumaryalla3@gmail.com>
2024-04-24 14:35:54 +0530

    deployed website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+// Importing some the of the dependiences from the react-router dom
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+// Importing Componets
+import NavBar from './Components/NavBar/Navbar';
+import Home from './Components/Home/Home';
+// Importing pages
+import AddToContact from './Pages/AddToContact/AddToContact';
+import Edit from './Pages/EditContact/EditContact';
+// Importing stateManagement Library
+import CustomeContext from './context';
+
+// Importing the Toastify to disply the notification in the react-app
+import { ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+
+// Importing css file
 import './App.css';
 
+
+
 function App() {
+
+  // Creating Routes
+  const router = createBrowserRouter([
+     {path : '/' , element : <NavBar /> , children : [
+      {path : '/' , element : <Home />},
+      {path : 'add-contact' , element : <AddToContact /> },
+      {path : 'edit-contact/:id' , element : <Edit />}
+     ]}
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CustomeContext>
+      <ToastContainer />
+      <div className="App">
+        {/* Assigning Routes */}
+        <RouterProvider router={router} />
+      
+      </div>
+    </CustomeContext>
   );
 }
 
